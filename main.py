c0f84d6bd0744522ee8a9b682fa10f7ec88eb8ef
bakinovsky-m <mbtrash@yandex.ru>
2017-05-12 11:52:03 +0300

    beautify

--- main.py
+++ main.py
@@ -8,35 +8,11 @@ from pygame import *
 WIN_H = 800
 WIN_W = 600
 DISPLAY = (WIN_H, WIN_W)
-BG_COLOR = "#FFFFFF"
+BG_COLOR = "#004400"
+FG_COLOR = "#FFFFFF"
 
-clock = pygame.time.Clock()
-screen = pygame.display.set_mode(DISPLAY)
-
-
-
-# class Vec2:
-# 	x = 0
-# 	y = 0
-
-# 	def __init__(self, x, y):
-# 		self.x = x
-# 		self.y = y
-
-# 	def __mul__(self, a):
-# 		return Vec2(self.x * a.x, self.y * a.y)
-
-# 	def __add__(self, a):
-# 		return Vec2(self.x + a.x, self.y + a.y)
-
-# 	def multiply(self, a):
-# 		return Vec2(self.x * a, self.y * a)
-
-# 	def retTuple(self):
-# 		return (self.x, self.y)
-
-# 	def retIntTuple(self):
-# 		return (int(self.x), int(self.y))
+# clock = pygame.time.Clock()
+# screen = pygame.display.set_mode(DISPLAY)
 
 def bezierPoint(t, p0, p1, p2, p3):
 	u = 1-t
@@ -45,25 +21,16 @@ def bezierPoint(t, p0, p1, p2, p3):
 	uuu = uu * u
 	ttt = tt * t
 
-	# p = Vec2(p0.x * uuu, p0.y * uuu)
-	# p += Vec2(p1.x * (3 * uuu * t), p1.y * (3 * uuu * t))
-	# p += Vec2(p2.x * (3 * u * tt), p2.y * (3 * u * tt))
-	# p += Vec2(p3.x * ttt, p3.y * ttt)
-
-	# return p.retIntTuple()
-	
-	# p = p0.x * uuu, p0.y * uuu
 	p = []
 	p.append(p0.x * uuu)
 	p.append(p0.y * uuu)
 
-	# p += p1.x * (3 * uuu * t), p1.y * (3 * uuu * t)
 	p[0] += p1.x * (3 * uuu * t)
 	p[1] += p1.y * (3 * uuu * t)
-	# p += p2.x * (3 * u * tt), p2.y * (3 * u * tt)
+
 	p[0] += p2.x * (3 * u * tt)
 	p[1] += p2.y * (3 * u * tt)
-	# p += p3.x * ttt, p3.y * ttt
+	
 	p[0] += p3.x * ttt
 	p[1] += p3.y * ttt
 
@@ -76,21 +43,22 @@ def drawAll(screen, bg, iters, dots, p0, p1, p2, p3):
 	count = 0
 	while count != iters:
 		# draw.circle(bg, Color("#FFFFFF"), dots[count], 1)
-		draw.rect(bg, Color("#FFFFFF"), dots[count], 1)
+		draw.rect(bg, Color(FG_COLOR), dots[count], 1)
 		count += 1
-	draw.rect(bg, Color("#FFFFFF"), p0, 1)
-	draw.rect(bg, Color("#FFFFFF"), p1, 1)
-	draw.rect(bg, Color("#FFFFFF"), p2, 1)
-	draw.rect(bg, Color("#FFFFFF"), p3, 1)
+	draw.rect(bg, Color(FG_COLOR), p0, 1)
+	draw.rect(bg, Color(FG_COLOR), p1, 1)
+	draw.rect(bg, Color(FG_COLOR), p2, 1)
+	draw.rect(bg, Color(FG_COLOR), p3, 1)
 
 def main():
 	pygame.init()
-#	screen = pygame.display.set_mode(DISPLAY)
+	clock = pygame.time.Clock()
+	screen = pygame.display.set_mode(DISPLAY)
 	pygame.display.set_caption("Bezier")
-	#bg = Surface(DISPLAY)
-	#bg.fill(Color(BG_COLOR))
-	bg = pygame.image.load("bg.jpg").convert()
-	bg = pygame.transform.scale(bg,DISPLAY)
+	bg = Surface(DISPLAY)
+	bg.fill(Color(BG_COLOR))
+	# bg = pygame.image.load("bg.jpg").convert()
+	# bg = pygame.transform.scale(bg,DISPLAY)
 	clock.tick(60)
 
 	p0 = Rect(0,0, 10, 10)
@@ -173,37 +141,23 @@ def main():
 					mouse_x, mouse_y = ev.pos
 					p1.x = mouse_x + offset_x
 					p1.y = mouse_y + offset_y
-					#bg.fill(Color(BG_COLOR))
+					bg.fill(Color(BG_COLOR))
 					drawAll(screen, bg, iters, dots, p0, p1, p2, p3)
 
 				elif mouse_dragging_2:
 					mouse_x, mouse_y = ev.pos
 					p2.x = mouse_x + offset_x
 					p2.y = mouse_y + offset_y
-					#bg.fill(Color(BG_COLOR))
+					bg.fill(Color(BG_COLOR))
 					drawAll(screen, bg, iters, dots, p0, p1, p2, p3)
 
 				elif mouse_dragging_3:
 					mouse_x, mouse_y = ev.pos
 					p3.x = mouse_x + offset_x
 					p3.y = mouse_y + offset_y
-					#bg.fill(Color(BG_COLOR))
+					bg.fill(Color(BG_COLOR))
 					drawAll(screen, bg, iters, dots, p0, p1, p2, p3)
 
-
-
-
-		# screen.fill(Color(BG_COLOR))
-		# screen.blit(bg, (0,0))
-		# count = 0
-		# while count != iters:
-		# 	# draw.circle(bg, Color("#FFFFFF"), dots[count], 1)
-		# 	draw.rect(bg, Color("#004400"), dots[count], 10)
-		# 	count += 1
-		# draw.rect(bg, Color("#FFFFFF"), p0, 1)
-		# draw.rect(bg, Color("#FFFFFF"), p1, 1)
-		# draw.rect(bg, Color("#FFFFFF"), p2, 1)
-		# draw.rect(bg, Color("#FFFFFF"), p3, 1)
 		drawAll(screen, bg, iters, dots, p0, p1, p2, p3)
 		pygame.display.flip()
 
